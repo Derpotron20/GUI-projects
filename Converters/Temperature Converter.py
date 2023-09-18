4b421e1bc07a7058d397438f3508dbac14cdfaac
Derpotroon20 <26bragam@student.pac.nsw.edu.au>
2023-09-19 09:01:27 +1000

    More Projects
    I uploaded more of my projects to github so that more people can access them

---
+++ Converters/Temperature Converter.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Temperature Converter")
+root.geometry("275x100")
+
+answer_C = None
+answer_F = None
+def show_message_C():
+    global answer_C
+    dgs_C = int(entry.get())
+    answer_C = round(((dgs_C * 9/5) + 32), 1)
+    label.config(text=answer_C)
+
+def show_message_F():
+    global answer_F
+    dgs_F = int(entry.get())
+    answer_F = round(((dgs_F - 32) * 5/9), 1)
+    label.config(text=answer_F)
+
+def toggle():
+    if check_state.get() == 0:
+        label_type1.config(text="°C")
+        label_type2.config(text="°F")
+
+    else:
+        label_type1.config(text="°F")
+        label_type2.config(text="°C")
+
+def msg_shrtcut(event):
+    if event.keysym == "Return" and check_state.get() == 0:
+        show_message_C()
+    elif event.keysym == "Return" and check_state.get() == 1:
+        show_message_F()
+
+entry = tk.Entry(root, width=5, font=('Arial', 16))
+entry.bind("<KeyPress>", msg_shrtcut)
+entry.place(x=13, y=10)
+
+label_arrow1 = tk.Label(root, text=("|\n" + "ˇ"))
+label_arrow1.place(x=33, y=40)
+
+check_state = tk.IntVar()
+
+check = tk.Checkbutton(root, text="Convert to °C instead", font=('Arial', 14), variable=check_state, command=toggle)
+check.place(x=95, y=13)
+
+
+label = tk.Label(root, text=answer_C)
+label.place(x=23, y=70)
+
+label_type1 = tk.Label(root, text="°C")
+label_type1.place(x=70, y=12)
+
+label_type2 = tk.Label(root, text="°F")
+label_type2.place(x=70, y=70)
+
+
+root.mainloop()
---
+++ Converters/Time Converter/Time Converter GUI.py
@@ -0,0 +1,147 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+root = tk.Tk()
+root.title("Time Converter")
+root.geometry("510x130")
+
+
+def msg_shrtcut(event):
+    print(event)
+    if event.keysym == "Return" and choice.get() == 1:
+        weeks_days()
+    elif event.keysym == "Return" and choice.get() == 2:
+        days_hours()
+    elif event.keysym == "Return" and choice.get() == 3:
+        hours_minutes()
+    elif event.keysym == "Return" and choice.get() == 4:
+        minutes_seconds()
+
+def weeks_days():
+    global rev
+    counter = 0
+    num = int(input.get())
+    # Weeks into Days
+    if rev.get() == 0:
+        num = num * 7
+        answer.config(text=f'{num} days')
+        
+    # Days into Weeks
+    elif rev.get() == 1:
+        while num > 7:
+            num -= 7
+            counter += 1
+        if num == 0:
+            answer.config(text=f'{counter} weeks')
+        elif num > 0:
+            answer.config(text=f'{counter} weeks and {num} days')
+
+def days_hours():
+    global rev
+    counter = 0
+    num = int(input.get())
+    # Days into Hours
+    if rev.get() == 0:
+        num = num * 24
+        answer.config(text=f'{num} hours')
+        
+    # Hours into Days
+    elif rev.get() == 1:
+        while num > 24:
+            num -= 24
+            counter += 1
+        if num == 0:
+            answer.config(text=f'{counter} days')
+        elif num > 0:
+            answer.config(text=f'{counter} days and {num} hours')
+
+def hours_minutes():
+    global rev
+    counter = 0
+    num = int(input.get())
+    # Hours into Minutes
+    if rev.get() == 0:
+        num = num * 60
+        answer.config(text=f'{num} hours')
+        
+    # Minutes into Hours
+    elif rev.get() == 1:
+        while num > 60:
+            num -= 60
+            counter += 1
+        if num == 0:
+            answer.config(text=f'{counter} hours')
+        elif num > 0:
+            answer.config(text=f'{counter} hours and {num} minutes')
+
+def minutes_seconds():
+    global rev
+    counter = 0
+    num = int(input.get())
+    # Minutes into Seconds
+    if rev.get() == 0:
+        num = num * 60
+        answer.config(text=f'{num} minutes')
+        
+    # Seconds into Minutes
+    elif rev.get() == 1:
+        while num > 60:
+            num -= 60
+            counter += 1
+        if num == 0:
+            answer.config(text=f'{counter} minutes')
+        elif num > 0:
+            answer.config(text=f'{counter} minutes and {num} seconds')
+
+def toggle():
+    if rev.get() == 0:
+        option1.config(text="Weeks to Days")
+        option2.config(text="Days to Hours")
+        option3.config(text="Hours to Minutes")
+        option4.config(text="Minutes to Seconds")
+
+    else:
+        option1.config(text="Days to Weeks")
+        option2.config(text="Hours to Days")
+        option3.config(text="Minutes to Hours")
+        option4.config(text="Seconds to Minutes")
+
+__name__ = "__main__"
+if __name__ == "__main__":
+    choice = tk.IntVar()
+
+    option1 = tk.Radiobutton(root, text="Weeks to Days", font=('Arial', 13), variable=choice, value=1)
+    option1.place(x=10, y=50)
+
+    option2 = tk.Radiobutton(root, text="Days to Hours", font=('Arial', 13), variable=choice, value=2)
+    option2.place(x=150, y=50)
+
+    option3 = tk.Radiobutton(root, text="Hours to Minutes", font=('Arial', 13), variable=choice, value=3)
+    option3.place(x=10, y=90)
+
+    option4 = tk.Radiobutton(root, text="Minutes to Seconds", font=('Arial', 13), variable=choice, value=4)
+    option4.place(x=150, y=90)
+
+    input = tk.Entry(root, font=('Arial', 15))
+    input.bind("<KeyPress>", msg_shrtcut)
+    input.place(x=10, y=10)
+
+    answer = tk.Label(root, text="", font=('Arial', 14))
+    answer.place(x=330, y=13)
+
+    arrow = Image.open("arrow.png")
+
+    resizedarrow = arrow.resize((60,18), Image.LANCZOS)
+
+    properarrow = ImageTk.PhotoImage(resizedarrow)
+
+    properarrowlabel = tk.Label(root, width=60, height=18, image=properarrow)
+    properarrowlabel.place(x=240, y=15)
+
+    rev = tk.IntVar()
+
+    reverse = tk.Checkbutton(root, text="Would you like to reverse?", font=('Arial', 13), variable=rev, command=toggle)
+    reverse.place(x=310, y=67)
+
+root.mainloop()
